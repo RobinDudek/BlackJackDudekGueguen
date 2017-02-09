590efdf1271cc70f7dfd621114c9325c165bc69c
RobinDudek <robin.dudek@ynov.com>
2017-02-09 16:11:16 +0100

    update deck

--- BlackJackDudekGueguen/Models/Card.cs
+++ BlackJackDudekGueguen/Models/Card.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BlackJackDudekGueguen.Model
+namespace BlackJackDudekGueguen.Models
 {
     class Card
     {
@@ -14,6 +14,29 @@ namespace BlackJackDudekGueguen.Model
 
         public Card(int color, int number)
         {
+            /**
+             * i ==> Color
+             * i = 1, SPADES (Pics)
+             * i = 2, HEARTS (Coeurs)
+             * i = 3, CLUBS (Trèfles)
+             * i = 4, DIAMONDS (Carreaux)
+             * 
+             * j ==> Number
+             * j = 1, ACE
+             * j = 2, TWO
+             * j = 3, THREE
+             * j = 4, FOUR
+             * j = 5, FIVE
+             * j = 6, SIX
+             * j = 7, SEVEN
+             * j = 8, EIGHT
+             * j = 9, NINE
+             * j = 10, TEN
+             * j = 11, JACK
+             * j = 12, QUEEN
+             * j = 13, KING
+             **/
+
             this.Color = color;
             this.Number = number;
             if(number==10 && number == 11 && number == 12 && number == 13)
--- BlackJackDudekGueguen/Models/Deck.cs
+++ BlackJackDudekGueguen/Models/Deck.cs
@@ -8,5 +8,34 @@ namespace BlackJackDudekGueguen.Models
 {
     class Deck
     {
+        public List<Card> Cards { get; set; }
+        public Deck()
+        {
+            this.Cards = new List<Card>();
+            CreateDeck();
+            ShuffleDeck();
+            AddCutCard();
+        }
+
+        public void CreateDeck()
+        {
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 13; j++)
+                {
+                    Cards.Add(new Card(i, j));
+                }
+            }
+        }
+
+        public void ShuffleDeck()
+        {
+
+        }
+
+        public void AddCutCard()
+        {
+
+        }
     }
 }
